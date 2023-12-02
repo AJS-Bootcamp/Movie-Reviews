67256567ab8764565dbfeaa880ec651e6726959a
aumoor02 <109328140+aumoor02@users.noreply.github.com>
2023-12-02 14:36:53 -0600

    Added Express Routes

--- server/index.js
+++ server/index.js
@@ -1,25 +1,21 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const apiRouter = require('./routes/api');
+const userRouter = require('./routes/userRouter');
+
 const hostname = 'localhost';
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 app.use(morgan('dev'));
 app.use(express.json());
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/client'));
 
-// app.use((req, res) => {
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/html');
-//   res.end('<html><body><h1>This is an Express Server</h1></body></html>');
-// });
-
-app.get('/api', (req, res) => {
-  res.json({ message: 'Hello from server!' });
-});
+app.use('/', apiRouter);
+app.use('/users', userRouter);
 
 app.listen(PORT, hostname, () => {
   console.log(`Server running at http://${hostname}:${PORT}/`);
