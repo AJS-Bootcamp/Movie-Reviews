36788f7f626e0c73cc0755de7264fb0e2cd8ca9b
aumoor02 <109328140+aumoor02@users.noreply.github.com>
2023-12-05 14:18:01 -0600

    Connected to MongoDB

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
+require('dotenv').config({ path: '.env' });
+
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 const apiRouter = require('./routes/api');
 const userRouter = require('./routes/userRouter');
@@ -7,7 +10,6 @@ const userRouter = require('./routes/userRouter');
 const movieRouter = require('./routes/movieRouter');
 
 const hostname = 'localhost';
-const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -19,8 +21,20 @@ app.use(express.static(__dirname + '/client'));
 app.use('/', apiRouter);
 app.use('/users', userRouter);
 
-app.listen(PORT, hostname, () => {
-  console.log(`Server running at http://${hostname}:${PORT}/`);
-});
-
 app.use('/api/movies', movieRouter);
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    writeConcern: { w: 'majority' },
+  })
+  .then(() => {
+    app.listen(process.env.PORT, hostname, () => {
+      console.log(
+        'Connected to database and listening on port',
+        process.env.PORT
+      );
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
