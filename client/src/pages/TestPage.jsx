7d98f0f9d0da48bc8382c20ff0fd300f3c993468
aumoor02 <109328140+aumoor02@users.noreply.github.com>
2024-01-23 16:19:08 -0600

    Movie Grid Template

--- client/src/pages/TestPage.jsx
+++ client/src/pages/TestPage.jsx
@@ -1,13 +1,81 @@
-import React from 'react';
-import TrendingList from '../components/movies/trendingList';
+import { useState, useEffect } from 'react';
+import styles from '../styles.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-regular-svg-icons';
+import { faCircleCheck } from '@fortawesome/free-regular-svg-icons';
 
+const TestMovie = ({ movie }) => {
+  const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
 
-const TestPage = () => {
-
+  const { poster_path, title, overview } = movie;
 
   return (
-    <div>
-        <TrendingList />
+    <div className={styles.movieContainer}>
+      <img
+        className={styles.moviePoster}
+        src={`${IMG_PATH + poster_path}`}
+        alt={`${title} Poster`}
+      />
+      <div className={styles.overlay}>
+        <div className={styles.movieDetails}>
+          <h3>{title}</h3>
+          <p>{overview}</p>
+          <div className={styles.icons}>
+            <FontAwesomeIcon className={styles.star} icon={faStar} size="2xl" />
+            <FontAwesomeIcon
+              className={styles.check}
+              icon={faCircleCheck}
+              size="2xl"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const TestPage = () => {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const getMovies = async () => {
+      const taskFromServer = await fetchData();
+      setMovies(taskFromServer);
+    };
+
+    getMovies();
+  }, []);
+
+  async function fetchData() {
+    try {
+      setLoading(true);
+      const response = await fetch('/api/movies');
+
+      if (!response.ok) {
+        throw new Error('Unable to fetch movies. Status: ', response.staus);
+      }
+
+      const data = await response.json();
+      console.log(data);
+      return data.results;
+    } catch (error) {
+      console.error('Error fetching data', error.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return (
+    <div className={styles.testPage}>
+      <h2 className={styles.movieSectionHeading}>Movies</h2>
+      <div className={styles.actionGrid}>
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          movies.map((movie) => <TestMovie key={movie.id} movie={movie} />)
+        )}
+      </div>
     </div>
   );
 };
