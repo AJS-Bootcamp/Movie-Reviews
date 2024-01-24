9c1696335192cdfcfe40a3447bb06bbef3efff57
aumoor02 <109328140+aumoor02@users.noreply.github.com>
2024-01-24 14:04:51 -0600

    Added Movies to Various Genre Pages

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,14 @@ import Favorites from '../src/components/movies/Favorites';
 import LoginPage from './pages/LoginPage';
 import TestPage from './pages/TestPage';
 import TrendingPage from './pages/TrendingPage';
+import ActionPage from './pages/ActionPage';
+import AnimationPage from './pages/AnimationPage';
+import ComedyPage from './pages/ComedyPage';
+import DramaPage from './pages/DramaPage';
+import FamilyPage from './pages/FamilyPage';
+import HorrorPage from './pages/HorrorPage';
+import RomancePage from './pages/RomancePage';
+
 
 function App() {
   return (
@@ -18,11 +26,18 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/trendingmovies" element={<TrendingPage />} />
+        <Route path="/actionmovies" element={<ActionPage />} />
+        <Route path="/animationmovies" element={<AnimationPage />} />
+        <Route path="/comedymovies" element={<ComedyPage />} />
+        <Route path="/dramamovies" element={<DramaPage />} />
+        <Route path="/familymovies" element={<FamilyPage />} />
+        <Route path="/horrormovies" element={<HorrorPage />} />
+        <Route path="/romancemovies" element={<RomancePage />} />
         <Route path="/User" element={<User />} />
         <Route path="LoginPage" element={<LoginPage />} />
         <Route path="/User/:userId/favorites" element={<Favorites />} />
         <Route path="/User/:userId/watchlist" element={<Watchlist />} />
-        <Route path="testpage" element={<TestPage />} />
+        <Route path="/testpage" element={<TestPage />} />
       </Routes>
 
       <Footer />
--- client/src/pages/RomancePage.jsx
+++ client/src/pages/RomancePage.jsx
@@ -1,54 +1,3 @@
-// import { useState, useEffect } from 'react';
-// import Movie from '../components/movies/Movie';
-
-// const TrendingPage = () => {
-//   const [trendingMovies, setTrendingMovies] = useState([]);
-//   const [loading, setLoading] = useState(false);
-
-//   useEffect(() => {
-//     const getTrendingMovies = async () => {
-//       const taskFromServer = await fetchData();
-//       setTrendingMovies(taskFromServer);
-//     };
-
-//     getTrendingMovies();
-//   }, []);
-
-//   async function fetchData() {
-//     try {
-//       setLoading(true); // Set loading state to true
-//       const response = await fetch('/api/movies/trending');
-//       if (!response.ok) {
-//         throw new Error('Network response was not ok');
-//       }
-//       const data = await response.json();
-//       console.log(data);
-//       return data;
-//     } catch (error) {
-//       console.log(`Error Fetching Data: ${error.message}`);
-//     } finally {
-//       setLoading(false); // Set loading state to false when done
-//     }
-//   }
-
-//   return (
-//     <div className='trending-page'>
-//       <h2>Trending Movies</h2>
-//       <div className="trending-grid">
-//         {loading ? (
-//           <p>Loading...</p>
-//         ) : (
-//           trendingMovies.map((trendingMovie) => (
-//             <Movie key={trendingMovie.id} movie={trendingMovie} />
-//           ))
-//         )}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default TrendingPage;
-
 import { useState, useEffect } from 'react';
 import styles from '../styles.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -85,7 +34,7 @@ const Movie = ({ movie }) => {
   );
 };
 
-const TrendingPage = () => {
+const RomancePage = () => {
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(false);
 
@@ -101,7 +50,7 @@ const TrendingPage = () => {
   async function fetchData() {
     try {
       setLoading(true);
-      const response = await fetch('/api/movies/trending');
+      const response = await fetch('/api/movies/romance');
 
       if (!response.ok) {
         throw new Error('Unable to fetch movies. Status: ', response.staus);
@@ -119,7 +68,7 @@ const TrendingPage = () => {
 
   return (
     <div className={styles.testPage}>
-      <h2 className={styles.movieSectionHeading}>Trending Movies</h2>
+      <h2 className={styles.movieSectionHeading}>Romance Movies</h2>
       <div className={styles.actionGrid}>
         {loading ? (
           <p>Loading...</p>
@@ -131,4 +80,4 @@ const TrendingPage = () => {
   );
 };
 
-export default TrendingPage;
+export default RomancePage;
